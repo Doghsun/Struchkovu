30f265ff44c62b1de42f8b15973997699030fc66
dev <dev@mail.com>
2020-05-12 08:55:36 +0900

    Updates

---
+++ precache-manifest.4fc54b06b7be90a88544af39f9c18a0c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fbaaadbc2be9497ab3c6aa0045e787",
+    "url": "/Struchkovu/index.html"
+  },
+  {
+    "revision": "b5879eefa726cbb0c5f8",
+    "url": "/Struchkovu/static/css/main.5865fbaf.chunk.css"
+  },
+  {
+    "revision": "2a0e3612e20cc48fcb38",
+    "url": "/Struchkovu/static/js/2.f6534ff3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Struchkovu/static/js/2.f6534ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5879eefa726cbb0c5f8",
+    "url": "/Struchkovu/static/js/main.6057dc98.chunk.js"
+  },
+  {
+    "revision": "7cf276eb148f4ebd20d9",
+    "url": "/Struchkovu/static/js/runtime-main.4e82d79d.js"
+  },
+  {
+    "revision": "46fa5d4491d7213d30e6bb9594b4653d",
+    "url": "/Struchkovu/static/media/Ctruchkova_Varvara.46fa5d44.png"
+  },
+  {
+    "revision": "f6449a2a7a52d1a4b292260152ea6b8a",
+    "url": "/Struchkovu/static/media/Cutar.f6449a2a.png"
+  },
+  {
+    "revision": "eda2281bfdb84f75c41d98ccae015aec",
+    "url": "/Struchkovu/static/media/Danil.eda2281b.png"
+  },
+  {
+    "revision": "27f101684409873c93baa9d308bf6566",
+    "url": "/Struchkovu/static/media/Luka.27f10168.png"
+  },
+  {
+    "revision": "4388335ff65bf106e5d769b72bb15261",
+    "url": "/Struchkovu/static/media/Maria_Gadril.4388335f.png"
+  },
+  {
+    "revision": "df98fd2c9a460770c08babf00f7075b6",
+    "url": "/Struchkovu/static/media/N_MarkovI.df98fd2c.png"
+  },
+  {
+    "revision": "e4ae03bf4465d474dbc1eecc3c356bab",
+    "url": "/Struchkovu/static/media/Vladimir_Gavril.e4ae03bf.png"
+  }
+]);
